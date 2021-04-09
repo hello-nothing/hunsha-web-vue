da9bcab8dcc2731685fb5db840cdb71d76280f7e
weirui <15037268535@sina.cn>
2021-04-09 14:55:52 +0800

    补充联系我们页面

--- src/components/footerBox.vue
+++ src/components/footerBox.vue
@@ -1,12 +1,9 @@
 <template>
   <div class="code-footer">
     <div class="module-list">
-      <div class="first-text">全国免费热线</div>
-      <div class="first-text">
-        15037268535
-      </div>
+      <div class="first-text">全国免费热线：15037268535</div>
     </div>
-    <div class="code-img-box first-img-box">
+    <div class="code-img-box">
       <div
         class="code-img"
         :style="{ backgroundImage: 'url(' + wxImg + ')' }"
@@ -31,40 +28,15 @@ export default {
 .code-footer {
   display: flex;
   justify-content: space-between;
-  padding: 50px 22%;
+  align-items: center;
+  width:calc(100% - 40%);
+  margin: 0 auto;
+  padding: 50px 20%;
   background-color: #d0d0d0;
 }
 .first-text {
-    padding-bottom: 20px;
   font-size: 16px;
   color: #000;
-}
-.first-img-box {
-  margin-right: 55px;
-}
-.phone-img {
-  position: absolute;
-  left: 0;
-  top: 21px;
-  width: 20px;
-  height: 20px;
-  background-repeat: no-repeat;
-  background-size: contain;
-  background-position: center;
-}
-.module-list {
-  padding-top: 26px;
-  padding-right: 70px;
-}
-.second-module {
-  display: flex;
-  align-items: center;
-  padding-top: 80px;
-}
-.address-text {
-  padding-left: 10px;
-  line-height: 16px;
-  border-left: 1px solid #000;
 }
 .code-img {
   width: 100px;
--- src/views/contactUs.vue
+++ src/views/contactUs.vue
@@ -1,10 +1,47 @@
 <template>
-    <div class="page-container">联系我们</div>
-  </template>
-  <script>
-  export default {
-    name: "contactUs"
-  };
-  </script>
-  <style lang="scss" scoped></style>
-  
+  <div class="page-container contact-content">
+    <div class="phone-text first-text">联系方式：15037268535</div>
+    <div class="phone-text">联系地址：河南省安阳市安阳工学院</div>
+    <div class="code-text">合作微信：</div>
+    <div
+      class="code-img"
+      :style="{ backgroundImage: 'url(' + wxImg + ')' }"
+    ></div>
+  </div>
+</template>
+<script>
+import wxImg from "../assets/wx.jpg";
+export default {
+  name: "contactUs",
+  data() {
+    return {
+      wxImg: wxImg
+    };
+  }
+};
+</script>
+<style lang="scss" scoped>
+.contact-content {
+  width: 60%;
+  margin: 0 auto;
+}
+.first-text {
+  padding-top: 30px;
+}
+.phone-text {
+  padding-bottom: 30px;
+  font-size: 18px;
+  font-weight: bold;
+}
+.code-text {
+  font-size: 18px;
+  font-weight: bold;
+}
+.code-img {
+  width: 200px;
+  height: 200px;
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position: center;
+}
+</style>
